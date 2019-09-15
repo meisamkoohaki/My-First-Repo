042f56c3e26225dca7f8304e0a0942e56314b7e8
Meisam Koohaki <200412272@student.georgianc.on.ca>
2019-09-15 11:35:01 -0400

    Initial code dommit

---
+++ My FIrst Web App/My FIrst Web App/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace My_FIrst_Web_App.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
